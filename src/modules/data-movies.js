24d25811aaa9cbc0de5209dab1c18835d89f96a3
Jhollyfer Rodrigues <jhollyferr@gmail.com>
2022-08-09 12:04:11 -0300

    :tada: initial setup & day 1

---
+++ src/modules/data-movies.js
@@ -0,0 +1,66 @@
+export const movies = [
+  {
+    image: "",
+    title: "Avengers Endgame (2019)",
+    points: 9.2,
+    isFavorite: false,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+  {
+    image: "",
+    title: "Batman (2022)",
+    points: 9.4,
+    isFavorite: true,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+  {
+    image: "",
+    title: "Doctor Strange in the Multiverse of Madness",
+    points: 8.5,
+    isFavorite: false,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+  {
+    image: "",
+    title: "Avatar (2009)",
+    points: 9.5,
+    isFavorite: true,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+  {
+    image: "",
+    title: "Avengers (2012)",
+    points: 9.2,
+    isFavorite: false,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+  {
+    image: "",
+    title: "Avengers Age of Ultron (2015)",
+    points: 9.0,
+    isFavorite: false,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+  {
+    image: "",
+    title: "Avengers Infinity War (2018)",
+    points: 9.6,
+    isFavorite: false,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+  {
+    image: "",
+    title: "Avengers Endgame (2019)",
+    points: 9.7,
+    isFavorite: false,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+];
